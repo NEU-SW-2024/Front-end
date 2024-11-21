249d495be22abd59ac9e4a95ace87bd64ce56103
skye <1429420771@qq.com>
2024-11-21 18:15:39 +0800

    4-fontend

--- src/api/system/dataAnalysis.js
+++ src/api/system/dataAnalysis.js
@@ -3,17 +3,8 @@ import request from '@/utils/request';
 // 获取项目列表
 export function getTenantProjects() {
     return request({
-        url: '/reports/projectInfo/1',
+        url: '/reports/projectInfo',
         method: 'get',
-    });
-
-}
-
-// 生成报告
-export function generateReports(projectId) {
-    return request({
-        url: '/audit/add/'+ projectId,
-        method: 'post',
     });
 
 }
